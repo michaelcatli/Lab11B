3efd97d31a08b70f93c003de81efa0f403e47da2
Mr Kiedes <akiedes@vtsd.com>
2020-01-22 08:28:05 -0500

    Starter Files

---
+++ Card.java
@@ -0,0 +1,42 @@
+// Card.java
+// This Card class is identical to the Card03 class.
+// It will be used for the sequence of Deck class programs.
+// //////////////////////////////////////////////////////////
+// This program is designed to explain the "Elevens" lab created
+// by Michael Clancy, Robert Glen Martin and Judith Hromcik
+
+
+
+public class Card
+{
+   private String suit;
+   private String rank;
+   private int value;
+
+   public Card(String s, String r, int v)
+   {
+      suit = s;
+      rank = r;
+      value = v;
+   }
+
+   public String getSuit()       { return suit; }
+   public String getRank()       { return rank; }
+   public int getValue()         { return value; }
+
+   public void setSuit(String s) { suit = s; }
+   public void setRank(String r) { rank = r; }
+   public void setValue(int v)   { value = v; }
+
+   public String toString()
+   {
+      return "[" + suit + ", " + rank + ", " + value + "]";
+   }
+
+   public boolean matches(Card otherCard)
+   {
+		return otherCard.getSuit().equals(this.suit)
+			 && otherCard.getRank().equals(this.rank)
+			 && otherCard.getValue() == this.value;
+	}
+}
