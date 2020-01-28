6d20defcaa98c3136edd79547651f346be6a0cff
Catli <10312@VTSD.COM>
2020-01-28 09:28:34 -0500

    oh gosh

--- Lab11bvst.java
+++ Lab11bvst.java
@@ -1,25 +1,54 @@
 // Lab11bst.java
-// This is the Student starting version of the <Deck> class lab11b assignment. 
+// This is the Student starting version of the <Deck> class lab11b assignment.
 
 
-public class Lab11bst
+public class Lab11bvst
 {
 	public static void main(String[] args)
-	{ 
+	{
       Deck deck = new Deck();
       System.out.println(deck);
 	}
 }
 
 
-class Deck 
+class Deck
 {
 	private Card[] cards;
 	private int size;
-
-	public Deck() 
+	private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	private int[] values = {1,2,3,4,5,6,7,8,9,10,11};
+	private String[] ranks = {"1","2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+	public Deck()
    {
-      size = 52;
+    size = 52;
 		cards = new Card[size];
-	}    
+		for (int x=0;x<suits.length;x++){
+			for (int b=0;b<ranks.length;b++){
+				cards[b].setSuit(suits[x]);
+				cards[b].setRank(ranks[b]);
+				String rankworthy = cards[b].getRank();
+				if (rankworthy == "Jack" || rankworthy == "Queen" || rankworthy == "King"){
+					cards[b].setValue(10);
+				}
+				else if (rankworthy == "Ace"){
+					cards[b].setValue(11);
+				}
+				else{
+					int i = Integer.parseInt(rankworthy);
+					cards[b].setValue(i);
+				}
+			}
+		}
+	}
+
+	public String toString(){
+		String g = "";
+		for (int m = 0;m<size;m++){
+
+			g = g + cards[m].toString() + "\n";
+
+		}
+		return g;
+	}
  }
