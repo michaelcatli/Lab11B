67804eb7cc61cd710b6f39510b2a5eaeea965e6b
Catli <10312@vtsd.com>
2020-01-28 11:14:13 -0500

    bjkhv

--- Lab11bvst.java
+++ Lab11bvst.java
@@ -17,7 +17,8 @@ class Deck
 	private Card[] cards;
 	private int size;
 	private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-	private int[] values = {1,2,3,4,5,6,7,8,9,10,11};
+	private int[] values = {1,2,3,4,5,6,7,8,9,10,10,10,10,11};
+	private int currentCard = 0;
 	private String[] ranks = {"1","2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
 	public Deck()
    {
@@ -25,30 +26,18 @@ class Deck
 		cards = new Card[size];
 		for (int x=0;x<suits.length;x++){
 			for (int b=0;b<ranks.length;b++){
-				cards[b].setSuit(suits[x]);
-				cards[b].setRank(ranks[b]);
-				String rankworthy = cards[b].getRank();
-				if (rankworthy == "Jack" || rankworthy == "Queen" || rankworthy == "King"){
-					cards[b].setValue(10);
-				}
-				else if (rankworthy == "Ace"){
-					cards[b].setValue(11);
-				}
-				else{
-					int i = Integer.parseInt(rankworthy);
-					cards[b].setValue(i);
+				cards[currentCard] = new Card(suits[x], ranks[b],values[b]);
+				currentCard++;
 				}
 			}
 		}
-	}
+		public String toString(){
+			String g = "";
+			for (int m = 0;m<size;m++){
 
-	public String toString(){
-		String g = "";
-		for (int m = 0;m<size;m++){
+				g = g + cards[m].toString() + "\n";
 
-			g = g + cards[m].toString() + "\n";
-
+			}
+			return g;
 		}
-		return g;
-	}
- }
+	 }
